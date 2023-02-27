f48dafa9565ba9546d212abe3bc85ca5b355153d
Joshua Beh <joshbeh88@gmail.com>
2023-02-27 21:47:33 +0000

    Nothing achieved, went backwards.

--- client/components/all-images.jsx
+++ client/components/all-images.jsx
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function AllImages({ user }) {
+  const [images, setImages] = useState();
+
+  useEffect(() => {
+    // const options = {
+    //   method: 'GET',
+    //   headers: {
+    //     'Content-Type': 'application/json'
+    //   }
+    // };
+    fetch('/images')
+      .then(response => response.json())
+      .then(data => {
+        setImages(data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
   return (
-    <div>
-      <h1 className='white'>hi</h1>
+    <>
+      <div className='row center'>
+        <h1 className='green'>All Images</h1>
+      </div>
+      <div className='row'>
+
+        {
+            images && images.map(image => (
+              <div key={image.imageId} className=''>
+                <Image image={image}/>
+              </div>
+            ))
+          }
+
+      </div>
+    </>
+  );
+}
+
+function Image({ image }) {
+  return (
+    <div className='col-25'>
+      <img src={image.src} alt="" />
     </div>
   );
 }
